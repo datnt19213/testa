b6521efd0b97f20fbcaf8d5020d3b3e0cbf89419
datnt19213 <91647943+datnt19213@users.noreply.github.com>
2021-10-21 10:20:54 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
     $conn = pg_connect('postgres://lhfaeztmdritlk:196767882407a8320eeb5c75baa3a89ca95a1d8d491ce84db4c039565103be19@ec2-34-232-245-127.compute-1.amazonaws.com:5432/dbv1i1bscp30v3');
     
-    if(!$conn){echo "Cannot to connect the datasbase";}
+    if(!$conn){die "Cannot to connect the datasbase";}
 ?>
